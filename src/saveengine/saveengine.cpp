74a5ade8f0ddf7dc4771fc3bbb0eccdd1b387881
Kalbra <kalle.bracht@outlook.de>
2020-06-01 16:38:53 +0200

    :page_facing_up: Documentation for Saveengien.cpp

--- src/saveengine/saveengine.cpp
+++ src/saveengine/saveengine.cpp
@@ -5,36 +5,33 @@
 #include <QJsonObject>
 #include <QDebug>
 
-std::vector<Boardelement> Saveengine::GetFromFile(QString path){
-    std::vector<Boardelement> boardelements;
+std::vector<Boardelement> Saveengine::GetFromFile(QString path){      //Funktion um von einer Datei(Board Datei) in einen Vector(Boardelement) zu laden, dieser wird dann returnt.
+    std::vector<Boardelement> boardelements;                          //Definiert den Vetor der Klasse Boardelement(siehe: boardelements.h)
 
-    QJsonDocument document = LoadJsonDocument(path);
-    QJsonArray    array    = document.array();
+    QJsonDocument document = LoadJsonDocument(path);                  //Nimmt mit der Funktion LoadJsonDocument() das QJsonDocunent und speichert es in document
+    QJsonArray    array    = document.array();                        //Konvertiert document zu einem Array in array
 
-    //qDebug() << document.toJson();
+    for(int i = 0; i < array.size(); i++){                            //For Schleife um aus dem Array die Elemente zu extrahieren
+        QJsonObject element = array[i].toObject();                    //Speichert das jetzige Elemten in der Variable element
 
-    for(int i = 0; i < array.size(); i++){
-        QJsonObject element = array[i].toObject();
+        QString eventtype = element.value("type").toString();         //Extrahiert aus dem element das Keyword type und Konvertiert es in einen String
 
-        QString eventtype = element.value("type").toString();
+        if(eventtype == "button"){                                    //Wenn der Eventtype gleich button ist
+            Button button;                                            //Erstellt einen neuen Button
+            button.name   = element.value("name")  .toString();       //Übergibt den Namen für den Button
+            button.action = element.value("action").toString();       //Übergibt die Aktion, die ausgeführt werden soll
 
-        if(eventtype == "button"){
-            Button button;
-            button.name   = element.value("name")  .toString();
-            button.action = element.value("action").toString();
-
-            boardelements.push_back(button);
+            boardelements.push_back(button);                          //Hängt den Button an das Ende des Vectors(boardelements) an
         }
-        else if(eventtype == "slider"){
-            Slider slider;
-            slider.name   = element.value("name").toString();
-            slider.from   = element.value("from").toInt();
-            slider.to     = element.value("to")  .toInt();
+        else if(eventtype == "slider"){                               //Wenn der Eventtype gleich slider ist
+            Slider slider;                                            //Erstellt einen neuen Slider
+            slider.name   = element.value("name").toString();         //Übergibt den Namen für den Slider
+            slider.from   = element.value("from").toInt();            //Übergibt die From value für den Slider
+            slider.to     = element.value("to")  .toInt();            //Übergibt die To value für dne Slider
 
-            boardelements.push_back(slider);
+            boardelements.push_back(slider);                          //Hängt den Slider an das Ende des Vectors(boardelements) an
         }
     }
 
-
-    return boardelements;
+    return boardelements;                                             //Returnt den Vector(boardelements)
 }
