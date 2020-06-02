2632d72829075155b3e58da2bd914fd70c1e8fc5
Kalbra <kalle.bracht@outlook.de>
2020-06-02 19:17:45 +0200

    :page_facing_up: Documentation serialio
    documentation:
    - serialio.h
    - serialio.cpp

--- src/serialio/serialio.h
+++ src/serialio/serialio.h
@@ -6,14 +6,14 @@
 class Serialio
 {
 public:
-    Serialio(QString port, int baudrate);
+    Serialio(QString port, int baudrate);                             //Klass Constructur der den Port braucht und die Baudrate(nicht Verflichtend Standardwert: 9600)
 
-    QList<QSerialPortInfo> getPorts();
+    QList<QSerialPortInfo> getPorts();                                //Funktion um die verfügbaren Ports in eine QList(Vector) zu schreiben, diese wird dann returnt
 
-    void setPort(QString port);
-    void setBaudRate(int baudrate);
+    void setPort(QString port);                                       //Funktion um den Port zu ändern
+    void setBaudRate(int baudrate);                                   //Funktion um die Baudrate zu setzen (oder zu ändern)
 
-    void send(QString str);
+    void send(QString str);                                           //Funktion um einen String zu senden
 
 private:
     QString p_port;
