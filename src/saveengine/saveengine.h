13f5f16252429609de222aab94b12000e42dd8c2
Kalbra <kalle.bracht@outlook.de>
2020-06-06 13:18:40 +0200

    Add save to file on saveengine

--- src/saveengine/saveengine.h
+++ src/saveengine/saveengine.h
@@ -7,6 +7,7 @@ class Saveengine
 {
 public:
     std::vector<Boardelement> GetFromFile(QString path);              //Funktion um von einer Datei(Board Datei) in einen Vector(Boardelement) zu laden, dieser wird dann returnt.
+    void SaveToFile(QString path, std::vector<Boardelement> boardelements);
 };
 
 #endif // SAVEENGINE_H
