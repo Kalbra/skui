3d23776cdfa06ec00ec36a292001e218422d7b31
Kalbra <kalle.bracht@outlook.de>
2020-07-17 16:43:53 +0200

    :pencil2: Changing welcome structure

---
+++ src/welcome/welcome.cpp
@@ -0,0 +1,26 @@
+﻿#include "welcome.h"
+#include <QWidget>
+#include <QUrl>
+#include <QFileInfo>
+#include <QWebEngineView>
+#include <QVBoxLayout>
+#include <QObject>
+#include <QMainWindow>
+
+
+QWidget * Welcome::WelcomeMessage(QMainWindow *window){
+    QWidget *widget = new QWidget();
+
+    QWebEngineView *webengine = new QWebEngineView(widget);
+
+    webengine->setUrl(QUrl("qrc:/html/welcome.html"));
+
+    connect(webengine, SIGNAL(urlChanged(const QUrl &url)), window, SLOT([]{qDebug() << "lsdfjk";}));
+
+
+    QVBoxLayout *layout = new QVBoxLayout(widget);
+    layout->setMargin(0);
+    layout->addWidget(webengine);
+
+    return widget;
+}
